20cd3b3d46de5b170307d63426c0c7fa7a08d71a
Кирилл Музыкантов <dedisdead666@mail.ru>
2025-11-27 11:20:26 +0500

    автообрезка фото 512х512 пнг

--- Backend/StudentHub.Infrastructure/Services/FileStorage.cs
+++ Backend/StudentHub.Infrastructure/Services/FileStorage.cs
@@ -1,5 +1,8 @@
-﻿using StudentHub.Application.DTOs;
+﻿using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp;
+using StudentHub.Application.DTOs;
 using StudentHub.Application.Interfaces.Services;
+using SixLabors.ImageSharp.Processing;
 
 namespace StudentHub.Infrastructure.Services
 {
@@ -13,13 +16,11 @@ namespace StudentHub.Infrastructure.Services
         }
         public async Task<Result<string>> SaveFileAsync(Stream fileStream, string fileName)
         {
-            var uniqueName = $"{Guid.NewGuid()}_{fileName}";
+            var uniqueName = $"{Guid.NewGuid()}_{fileName}.png";
             var fullPath = Path.Combine(_basePath, uniqueName);
+            var image = await ProcessImage(fileStream);
 
-            using (var stream = new FileStream(fullPath, FileMode.Create))
-            {
-                await fileStream.CopyToAsync(stream);
-            }
+            File.WriteAllBytes(fullPath, image);
 
             return Result<string>.Success(uniqueName);
         }
@@ -33,6 +34,22 @@ namespace StudentHub.Infrastructure.Services
 
             return Result<Stream>.Success(File.OpenRead(fullPath));
         }
+        
+        private async Task<byte[]> ProcessImage(Stream fileStream)
+        {
+            using var image = await Image.LoadAsync(fileStream);
 
+            int size = Math.Min(image.Width, image.Height);
+
+            int x = (image.Width - size) / 2;
+            int y = (image.Height - size) / 2;
+
+            image.Mutate(i => i.Crop(new Rectangle(x, y, size, size))); //вырезаем квадрат из центра
+            image.Mutate(i => i.Resize(512, 512)); //сжимаем
+
+            using var ms = new MemoryStream();
+            await image.SaveAsPngAsync(ms);
+            return ms.ToArray();
+        }
     }
 }
