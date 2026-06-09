09650e942065c59f2b60a0412976c5453d30b317
muzykantov.kd2741 <muzykantov.kd2741@PC-12-ROOM407>
2026-06-09 14:43:37 +0500

    Migration

---
+++ Backend/StudentHub.Infrastructure/Migrations/Business/20260609094241_idk44423.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentHub.Infrastructure.Migrations.Business
+{
+    /// <inheritdoc />
+    public partial class idk44423 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CriterionScores_TeacherId",
+                schema: "Business",
+                table: "CriterionScores",
+                column: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CriterionScores_Users_TeacherId",
+                schema: "Business",
+                table: "CriterionScores",
+                column: "TeacherId",
+                principalSchema: "Business",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CriterionScores_Users_TeacherId",
+                schema: "Business",
+                table: "CriterionScores");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CriterionScores_TeacherId",
+                schema: "Business",
+                table: "CriterionScores");
+        }
+    }
+}
